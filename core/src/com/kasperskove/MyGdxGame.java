a1147df851daa897cbcf06969b59b6ddd10fa667
kasper95 <kove_kasper@outlook.com>
2017-05-09 19:59:09 -0400

    +image movement method

--- core/src/com/kasperskove/MyGdxGame.java
+++ core/src/com/kasperskove/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.kasperskove;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -19,8 +20,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		x++;
-		y++;
+		move();
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -34,4 +34,22 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+	void move () {
+	    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+	        y++;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            y--;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            x++;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            x--;
+        }
+    }
 }
